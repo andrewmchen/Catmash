d4f27192d9256d788047f10f2c56ef5c0ddde397
Tony Duan <tony@tonyduan.com>
2013-09-17 00:43:46 -0700

    Crazy reorganization; removed database.

--- CatMash/urls.py
+++ CatMash/urls.py
@@ -1,22 +1,4 @@
-from django.conf.urls import patterns, include, url
-import os
+from django.conf.urls import patterns,url
+import views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', include('picture.urls')),
-    url(r'^picture$', include('picture.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.getenv('STATIC_DIR')})
-
-
-    # url(r'^CatMash/', include('CatMash.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns('',url(r'^picture$','views.allah'),url(r'^$',"views.index"))
