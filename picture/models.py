beb138d25ed401f46bed6a5f905a382d9c6cf226
andrewmchen <andrewm.chen@yahoo.com>
2013-09-18 14:33:42 -0700

    Improved the layout of the html document, added rating functionality inside of views.py, basic working model of catmash. TODO: fix rating (some bug in it), create CSS

--- picture/models.py
+++ picture/models.py
@@ -8,10 +8,3 @@ class pictures(models.Model):
     rating = models.IntegerField()
     def __unicode__(self):
         return self.url+' rating of '+str(self.rating)
-def fetchpictures(url):
-    html=urllib2.urlopen(url).read()
-    picturelist=re.findall(r'http://i.imgur.com.............',html)
-    for picture in picturelist:
-        p=pictures(url=picture,rating=1200)
-        p.save()
-fetchpictures('http://imgur.com/r/cats')
--- static/js/script.js
+++ static/js/script.js
@@ -10,9 +10,18 @@ $(document).ready(function(){
                 {
                     var picture1=$("#picture1 img").attr("src");
                     var picture2=$("#picture2 img").attr("src"); 
-                    var returninfo=picture1+"|"+picture2 // change to object to make less janky later
+                    var returninfo="picture1="+picture1+"&picture2="+picture2 // change to object to make less janky later
                     $('#selection').load("picture #selection",returninfo,function() {start()} ); //
                 }//function
-            )//live
+            )//click
+            $( "#picture2" ).delegate(this,'click',function()
+                {
+                    var picture1=$("#picture1 img").attr("src");
+                    var picture2=$("#picture2 img").attr("src"); 
+                    var returninfo="picture1="+picture2+"&picture2="+picture1 // change to object to make less janky later
+                    $('#selection').load("picture #selection",returninfo,function() {start()} ); //
+                }//function
+            )//lclick
+
         }//function
     //)
