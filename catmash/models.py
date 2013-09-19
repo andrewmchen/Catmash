562875472e791a32e793f8ef6688d4eb8dfc4717
Nathan <nathan@berkeley.edu>
2013-09-18 23:04:04 -0700

    fixed elo system, made elo a decimalfield, ensured two different cats each time

--- catmash/models.py
+++ catmash/models.py
@@ -5,6 +5,6 @@ import re
 # Create your models here.
 class pictures(models.Model):
     url = models.CharField(max_length=40)
-    rating = models.IntegerField()
+    rating = models.DecimalField(max_digits=10, decimal_places=5)
     def __unicode__(self):
         return self.url+' rating of '+str(self.rating)
--- catmash/views.py
+++ catmash/views.py
@@ -2,37 +2,28 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+
+from catmash.models import pictures
+from catmash.eloscript import adjust
+
 import string
 import random
 import re
 
-from catmash.models import pictures
 def index(request):
-    latest_picture_list=pictures.objects.all().order_by('?')[:2]
-    picture2=latest_picture_list[:1].get()
-    picture1=latest_picture_list[:1].get()
-    template=loader.get_template('catmash/index.html/')
+    latest_picture_list = pictures.objects.all().order_by('?')[:2]
+    picture1 = picture2 = 0
+    while picture1 == picture2:
+        picture2 = latest_picture_list[:1].get()
+        picture1 = latest_picture_list[:1].get()
+    template = loader.get_template('catmash/index.html/')
     print "index"
-    context= { "picture1": picture1,"picture2":picture2}
-    return render(request, 'catmash/index.html',context)
+    context = {"picture1":picture1, "picture2":picture2}
+    return render(request, 'catmash/index.html', context)
+
 def rate(request):
-    def adjust(winner,loser): #parameters are the urls
-        winner_object=pictures.objects.filter(url=winner)[0]
-        loser_object=pictures.objects.filter(url=loser)[0]
-        winner_rating=winner_object.rating
-        loser_rating=loser_object.rating
-        winner_expected=1.0/(1+10**((loser_rating-winner_rating)/400))
-        loser_expected=1.0/(1+10**((winner_rating-loser_rating)/400))
-        winner_new=winner_rating+32*(1-winner_expected)
-        loser_new=loser_rating+32*(0-loser_expected)
-        winner_object.rating=winner_new
-        loser_object.rating=loser_new
-        print winner_new
-        print loser_new
-        winner_object.save()
-        loser_object.save()
-    info=request.GET
-    
+    info = request.GET
     print info.__getitem__('picture1')
-    adjust(info.__getitem__('picture1'),info.__getitem__('picture2'))
+    adjust(info.__getitem__('picture1'), info.__getitem__('picture2'))
+    print info.__getitem__('picture1')
     return index(request)
