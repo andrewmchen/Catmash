f57725f1fc8cfcaacb596b8f9d53e19af395e786
andrewmchen <andrewm.chen@yahoo.com>
2013-09-18 23:14:09 -0700

    added top cats

--- catmash/views.py
+++ catmash/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 import string
 import random
 import re
-
 from catmash.models import pictures
+
 def index(request):
     latest_picture_list=pictures.objects.all().order_by('?')[:2]
     picture2=latest_picture_list[:1].get()
@@ -15,6 +15,18 @@ def index(request):
     print "index"
     context= { "picture1": picture1,"picture2":picture2}
     return render(request, 'catmash/index.html',context)
+def top(request):
+    number=10   #number of entries to display
+    info = request.GET
+    try: #try getting a number info item
+        starting = int(info.__getitem__('number'))
+        print starting
+        pictures_by_rating = pictures.objects.order_by('rating')[starting:starting+10]
+    except:
+        print "did not get request"
+        pictures_by_rating = pictures.objects.order_by('rating')[:10]
+    context = { "pictures_by_rating": pictures_by_rating}
+    return render(request, 'catmash/top.html', context)
 def rate(request):
     def adjust(winner,loser): #parameters are the urls
         winner_object=pictures.objects.filter(url=winner)[0]
