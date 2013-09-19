6793cf4103647b76f82fcb009323ed8c1bf7a459
Nathan <nathan@berkeley.edu>
2013-09-19 01:04:37 -0700

    fixed gui ugh

--- static/js/script.js
+++ static/js/script.js
@@ -6,18 +6,18 @@ $(document).ready(function(){
         function start()
         {
             console.log("ready")
-            $( "#picture1" ).delegate(this,'click',function()
+            $( ".picture1" ).delegate(this,'click',function()
                 {
-                    var picture1=$("#picture1 img").attr("src");
-                    var picture2=$("#picture2 img").attr("src"); 
+                    var picture1=$(".picture1").attr("src");
+                    var picture2=$(".picture2").attr("src");
                     var returninfo="picture1="+picture1+"&picture2="+picture2 // change to object to make less janky later
                     $('#selection').load("catmash #selection",returninfo,function() {start()} ); //
                 }//function
             )//click
-            $( "#picture2" ).delegate(this,'click',function()
+            $( ".picture2" ).delegate(this,'click',function()
                 {
-                    var picture1=$("#picture1 img").attr("src");
-                    var picture2=$("#picture2 img").attr("src"); 
+                    var picture1=$(".picture1").attr("src");
+                    var picture2=$(".picture2").attr("src");
                     var returninfo="picture1="+picture2+"&picture2="+picture1 // change to object to make less janky later
                     $('#selection').load("catmash #selection",returninfo,function() {start()} ); //
                 }//function
