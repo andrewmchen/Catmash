d347b128e4bdef3b8d45af5637cea9047734655f
Nathan <nathan@berkeley.edu>
2013-09-19 17:38:41 -0700

    jquery animations and ui improvement

--- catmash/urls.py
+++ catmash/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns,url
 
 
 urlpatterns = patterns('',
-    url(r'^$','catmash.views.rate'),
+    url(r'^$', 'catmash.views.rate'),
+    url(r'^top$', 'catmash.views.top'),
+    url(r'^trending$', 'catmash.views.top', { 'field': '-clicks'}),
     #url(r'^$',"picture.views.index")
 )
--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,25 @@
 //$( document ).ready(
 $(document).ready(function(){
     start()
-}
-)
-        function start()
-        {
-            console.log("ready")
-            $( ".picture1" ).delegate(this,'click',function()
-                {
-                    var picture1=$(".picture1").attr("src");
-                    var picture2=$(".picture2").attr("src");
-                    var returninfo="picture1="+picture1+"&picture2="+picture2 // change to object to make less janky later
-                    $('#selection').load("catmash #selection",returninfo,function() {start()} ); //
-                }//function
-            )//click
-            $( ".picture2" ).delegate(this,'click',function()
-                {
-                    var picture1=$(".picture1").attr("src");
-                    var picture2=$(".picture2").attr("src");
-                    var returninfo="picture1="+picture2+"&picture2="+picture1 // change to object to make less janky later
-                    $('#selection').load("catmash #selection",returninfo,function() {start()} ); //
-                }//function
-            )//lclick
+})
 
-        }//function
-    //)
+function start()
+{
+    console.log("ready")
+    $(".picture").delegate(this,'click',function() {
+        var picture1 = $(this).attr("src");
+        // messy as shit but it condenses two identical functions
+        var picture2 = $("#"+(parseInt($(this).attr("class").split(" ").pop())=='pic1' ? 'pic2' : 'pic1')).attr("src");
+        // change to object to make less janky later
+        var returninfo = "picture1=" + picture1 + "&picture2=" + picture2
+        $(this).toggleClass("active")
+        setTimeout(function() {
+            $("#selection").slideUp(300);
+            $("#selection").load("catmash #selection", returninfo, function() {
+                $("#selection").fadeIn(400);
+                $(this).toggleClass("active");
+                start();
+            });
+        }, 1500);
+    })//click
+}//function
