ba72dc56af067d06ac3197f6451c67f89c90da12
Nathan <nathan@berkeley.edu>
2013-09-19 20:50:08 -0700

    hopefully fixed dir erroes

--- Catmash/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns,url
-
-
-urlpatterns = patterns('',
-    url(r'^$', 'catmash.views.rate'),
-    url(r'^top$', 'catmash.views.top'),
-    url(r'^trending$', 'catmash.views.top', { 'field': '-clicks'}),
-    #url(r'^$',"picture.views.index")
-)
