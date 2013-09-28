b0e7542f5f37d6a5e2a6114dbe76e2c3ac8cc2a3
andrewmchen <andrewm.chen@yahoo.com>
2013-09-27 19:23:31 -0700

    index page working now

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+        url(r'^/login','users.views.login'),
+        url(r'^/register','users.views.register'),
+        url(r'^/logout','users.views.logout'),
+        url(r'^/profile','users.views.profile')
+        )
