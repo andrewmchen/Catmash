b446fc5bcc3d17836d0a1f10a789e529c8c75a4d
andrewmchen <andrewm.chen@yahoo.com>
2013-09-27 19:56:51 -0700

    fixed my fuck up from yesterday

--- users/views.py
+++ users/views.py
@@ -1,1 +1,51 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.shortcuts import render 
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as loginmethod
+from django.contrib.auth import logout as logoutmethod
+
+
+from users.forms import UserCreateForm, LoginForm
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            if user.is_active:
+                loginmethod(request,user)
+                print "logged in"
+                return HttpResponseRedirect('/users/profile')
+    form = LoginForm()
+    return render(request,"registration/login.html", {'form':form})
+
+def register(request):
+    if request.method == 'POST':
+        info = request.POST
+        form = UserCreateForm()
+        form.initiate_with_args(info.__getitem__('username'),info.__getitem__('password'),info.__getitem__('verify'),info.__getitem__('email'))
+        if form.is_valid():
+            print "Form is valid"
+            new_user = form.save()
+            return HttpResponse.HttpResponseRedirect("/users/login")
+        failed = True #makes a fail pop up
+        print "Form Failed"
+    else:
+        failed = False
+        form = UserCreateForm()
+    return render(request, "registration/register.html", {'form':form,'failed':failed})
+
+
+def logout(request):
+    logoutmethod(request)
+    return HttpResponseRedirect("/")
+    
+
+@login_required
+def profile(request):
+    return render(request, "profile/index.html", {'username':request.user.username})
--- website/urls.py
+++ website/urls.py
@@ -7,23 +7,23 @@ import os
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    #url(r'^$', include('picture.urls')),
-    url(r'^catmash', include('catmash.urls')), #ajax fetch link
-    url(r'^top','catmash.views.top'),
-    url(r'^trending$', 'catmash.views.top', { 'field': '-clicks'}),
-    url(r'^static/(?P<path>.*)$',
-        'django.views.static.serve',
-        {'document_root': os.getenv('STATIC_DIR')}),
-    url(r'^users',include('users.urls')),
-    url(r'^$', 'catmash.views.index'),
+        # Examples:
+        #url(r'^$', include('picture.urls')),
+        url(r'^catmash', include('catmash.urls')), #ajax fetch link
+        url(r'^top','catmash.views.top'),
+        url(r'^trending$', 'catmash.views.top', { 'field': '-clicks'}),
+        url(r'^static/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': os.getenv('STATIC_DIR')}),
+        url(r'^users',include('users.urls')),
+        url(r'^$', 'catmash.views.index'),
 
 
-    # url(r'^CatMash/', include('CatMash.foo.urls')),
+        # url(r'^CatMash/', include('CatMash.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        # Uncomment the admin/doc line below to enable admin documentation:
+        # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+        # Uncomment the next line to enable the admin:
+        # url(r'^admin/', include(admin.site.urls)),
+        )
